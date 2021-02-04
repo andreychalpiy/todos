18ded69bf8542bb1071964c69a164191ffef155e
andreychalpiy <andrey@eggshaped.co.uk>
2021-02-04 15:00:18 +0200

    start

---
+++ functions.js
@@ -0,0 +1,113 @@
+let users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "age": 18,
+        "director": false,
+        "title": "Accountant",
+        "company": {
+            "id": 1,
+            "name": "Yodah"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "age": 33,
+        "director": false,
+        "title": "Manager",
+        "company": {
+            "id": 1,
+            "name": "Yodah"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "age": 24,
+        "director": false,
+        "title": "Accountant",
+        "company": {
+            "id": 2,
+            "name": "Soundtesting"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "age": 45,
+        "director": false,
+        "title": "Accountant",
+        "company": {
+            "id": 2,
+            "name": "Soundtesting"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "age": 50,
+        "director": false,
+        "title": "Manager",
+        "company": {
+            "id": 1,
+            "name": "Yodah"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Dennis Schulist",
+        "age": 33,
+        "director": true,
+        "title": "Director",
+        "company": {
+            "id": 3,
+            "name": "Baseenergy"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "age": 27,
+        "director": false,
+        "title": "Accountant",
+        "company": {
+            "id": 4,
+            "name": "Thecodestore"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir",
+        "age": 27,
+        "director": false,
+        "title": "Accountant",
+        "company": {
+            "id": 1,
+            "name": "Yodah"
+        }
+    },
+    {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "age": 50,
+        "director": true,
+        "title": "Director",
+        "company": {
+            "id": 1,
+            "name": "Yodah"
+        }
+    },
+    {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "age": 49,
+        "director": true,
+        "title": "Director",
+        "company": {
+            "id": 2,
+            "name": "Soundtesting"
+        }
+    },
+];
+
