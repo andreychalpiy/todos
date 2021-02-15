f40ef6cded4a7172cf57fc0070bdb21e718f275d
andreychalpiy <andrey@eggshaped.co.uk>
2021-02-15 10:10:25 +0200

    func № 2,6,9,11,14,15

--- lodash_functions.js
+++ lodash_functions.js
@@ -135,13 +135,13 @@ let userSettings = [
 // 7. function getFirstNames() -> return all users first names ['Leanne', 'Ervin',...]
 function getFirstNames() {
     return _.map(users, function (user) {
-        return user.name.split(' ')[0];
+        return _.split(user.name, ' ')[0];
     })
 }
 // 8. function getLastNames() -> return all users last names ['Graham', 'Howell',...]
 function getLastNames() {
     return _.map(users, function (user) {
-        return user.name.split(' ')[1];
+        return _.split(user.name, ' ')[1];
     })
 }
 // 10. function maxUserAge() -> return max user age
@@ -150,7 +150,7 @@ function maxUserAge() {
 }
 // sort director true or false
 function getUsersByDirector(value) {
-    return _.filter(users, ['director', value])
+    return _.filter(users, ['director', value]);
 }
 // 3. function directorsAgeSum() -> return sum of ages where director=true
 function directorsAgeSum() {
@@ -166,5 +166,66 @@ function averageStaffAge() {
 }
 // 5. function getCompanyUsers(companyId) -> return users where company id = companyId
 function getCompanyUsers(companyId) {
-    return _.filter(users, ['id', companyId])
+    return _.filter(users, ['company.id', companyId]);
+}
+// 12. function sortByUsersCount() -> return (sorted by count of users) result from function groupByCompany()
+function sortByUsersCount() {
+    return _.sortBy(groupByCompany(), ['users.length']);
+}
+//6. function groupByCompany() -> return list of companies with users
+function groupByCompany() {
+    let comp = _.uniqBy(_.map(users, 'company'), "id");
+    _.map(comp, function (user) {
+        return _.each(user.users = getCompanyUsers(user.id), function (user) {
+            delete user.company;
+        })
+    })
+    return comp;
+}
+// // 11. function biggestCompanyName() -> return the company name with the most users
+function biggestCompanyName() {
+    return _.reverse(_.sortBy(groupByCompany(), ['users.length'])).splice(0, 1)[0].name
+}
+// 14. function usersWithSettings() -> return all users with added user settings, if no settings add empty settings settings:{}
+function usersWithSettings() {
+    return _.map(users, function (user) {
+        return _.assign({}, user, { setting: getUserSettings(user.id) });
+    })
+}
+//return user settings
+function getUserSettings(userId) {
+    let index = _.findIndex(userSettings, ['user_id', userId]);
+    return (index !== -1) ? userSettings[index].settings : {};
+}
+// 15. function getUsersByTheme(theme) -> return all users where settings.theme = theme
+function getUsersByTheme(theme) {
+    return _.filter(usersWithSettings(), ["setting.theme", theme]);
+}
+// 9. function groupByTitle() -> return list of titles with users list
+function groupByTitle() {
+    let sortTitle = _.uniqBy(_.map(users, function (user) {
+        return { title: user.title };
+    }), "title");
+    _.each(sortTitle, function (user) {
+        return _.assign(user, { users: getTitleUsers(user.title) });
+    })
+    return sortTitle;
+}
+// return users title
+function getTitleUsers(title) {
+    return _.filter(users, ["title", title]);
+}
+// 2. function filterAge(minAge, maxAge) -> return list where age >= minAge and age <= maxAge
+function filterAge(minAge, maxAge) {
+    return _.filter(users, function (user) {
+        return user.age >= minAge && user.age <= maxAge
+    });
+}
+// 13. function getCompaniesNameWithUsersCount() -> return new list {company_name: users_count,...} example {'Yodah': 3, 'Soundtesting': 2,...}
+function getCompaniesNameWithUsersCount() {
+    let arr = {};
+    _.each(groupByCompany(), function (item) {
+        arr[item.name] = item.users.length;
+    })
+    return arr;
 }
